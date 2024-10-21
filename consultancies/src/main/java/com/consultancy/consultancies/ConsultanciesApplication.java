ecb96da1afad65e88cde04158717978d25eb4c84
VieraJulian <julianviera2019@hotmail.com>
2024-10-21 19:56:46 -0300

    first commit

---
+++ consultancies/src/main/java/com/consultancy/consultancies/ConsultanciesApplication.java
@@ -0,0 +1,13 @@
+package com.consultancy.consultancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultanciesApplication.class, args);
+	}
+
+}
---
+++ consultancies/src/test/java/com/consultancy/consultancies/ConsultanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultancy.consultancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
